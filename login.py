508fc3b9750ac013dad1d8ff3a555f576228c8b3
edw7777 <wanged1110@gmail.com>
2025-01-20 17:29:48 -0500

    changes for aws deployment

--- login.py
+++ login.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://<your-amplify-url>.amplifyapp.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
