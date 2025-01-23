ee44f522cac390c2cc838fa7c828eacb1068610d
edw7777 <wanged1110@gmail.com>
2025-01-23 18:46:08 -0500

    Cors permissions tweak

--- login.py
+++ login.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://<your-amplify-url>.amplifyapp.com"],
+    allow_origins=["https://main.d3iovjyuh6mk6f.amplifyapp.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
